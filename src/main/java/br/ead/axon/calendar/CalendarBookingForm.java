ca689c8db019b1b9a7d2402f80b2f42237235ac0
Daniel Machado Vasconcelos <danielm.vasconcelos@gmail.com>
2026-02-26 21:10:06 +0100

    Add Thymeleaf HTMX calendar playground UI

---
+++ src/main/java/br/ead/axon/calendar/CalendarBookingForm.java
@@ -0,0 +1,50 @@
+package br.ead.axon.calendar;
+
+public class CalendarBookingForm {
+
+    private String anchorDate;
+    private String title;
+    private String startAt;
+    private String endAt;
+    private String colorToken;
+
+    public String getAnchorDate() {
+        return anchorDate;
+    }
+
+    public void setAnchorDate(String anchorDate) {
+        this.anchorDate = anchorDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStartAt() {
+        return startAt;
+    }
+
+    public void setStartAt(String startAt) {
+        this.startAt = startAt;
+    }
+
+    public String getEndAt() {
+        return endAt;
+    }
+
+    public void setEndAt(String endAt) {
+        this.endAt = endAt;
+    }
+
+    public String getColorToken() {
+        return colorToken;
+    }
+
+    public void setColorToken(String colorToken) {
+        this.colorToken = colorToken;
+    }
+}
